cc49f4019bee4174573ce8676103a7d831f60c6e
aravelha <rahelveralda@gmail.com>
2025-06-14 15:56:35 +0700

    initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== MOVIE TICKET BOOKING SYSTEM ===");
+        System.out.println("1. View Movies");
+        System.out.println("2. Book Ticket");
+        System.out.println("3. View Orders");
+        System.out.println("0. Exit");
+    }
+}
