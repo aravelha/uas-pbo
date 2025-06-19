d68e54535b408b7ce050e0b7ce8f0af4d4059f5c
aravelha <rahelveralda@gmail.com>
2025-06-19 09:10:17 +0700

    updating payment

--- src/model/payment/CashPayment.java
+++ src/model/payment/CashPayment.java
@@ -9,7 +9,6 @@ public class CashPayment implements PaymentMethod {
     @Override
     public int calculatePayment(int price, int qty){
         int totalPrice = price * qty;
-        System.out.printf("Total Pembelian tiket adalah Rp%,d untuk %d tiket%n", totalPrice, qty);
         return totalPrice;
     }
 }
